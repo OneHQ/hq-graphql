06d98be32d9c3cac68af1b88a7a837082f4855a5
Oscar Cristaldo <fabianskier@icloud.com>
2022-12-14 13:21:08 -0300

    [PD-9719]: auto add the graphql_name - EnumType
    Signed-off-by: Oscar Cristaldo <fabianskier@icloud.com>

--- lib/hq/graphql/ext/enum_extensions.rb
+++ lib/hq/graphql/ext/enum_extensions.rb
@@ -74,8 +74,10 @@ module HQ::GraphQL
         to_s.sub(/^((::)?\w+)::/, "")
       end
 
+      # This override method allow us to keep the suffix `-Type`
+      # if we don't specify the `graphql_name`.
       def default_graphql_name
-        to_s.split("::").last
+        to_s.split("::").last.sub(/\Z/, "")
       end
     end
   end
