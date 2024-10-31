95f993b0f000a8ccf9ac94a4bd3b1fd338914cbc
neryam3 <neryagueromatz@gmail.com>
2024-10-31 18:00:10 -0300

    organization updated for specs

--- spec/internal/app/models/manager.rb
+++ spec/internal/app/models/manager.rb
@@ -1,8 +1,9 @@
 class Manager < ::ActiveRecord::Base
-  belongs_to :organization
+  belongs_to :organization, class_name: "::HasHelpers::Organization"
 
   has_many :active_users, -> { where(inactive: [nil, false]) }, class_name: "::HasHelpers::User"
-  has_many :users, inverse_of: :organization
+  # has_many :users, inverse_of: :organization
+  has_many :users, class_name: "::HasHelpers::User"
   has_many :advisors, through: :users
   has_many :not_joe, -> { where.not(name: "Joe" ) }, through: :users, source: :advisor
 end
--- spec/internal/app/models/user.rb
+++ spec/internal/app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ::ActiveRecord::Base
   belongs_to :advisor, optional: true
   belongs_to :manager, optional: true
-  belongs_to :organization
+  has_many :user_organizations, dependent: :destroy, class_name: "::HasHelpers::UserOrganization"
+  has_many :organizations, class_name: "::HasHelpers::Organization", through: :user_organizations
 
   def hydrate
   end
