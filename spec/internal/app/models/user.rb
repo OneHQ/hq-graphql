3445ed542a3672cb8ffc0f787dba6b6cc77c99eb
neryam3 <neryagueromatz@gmail.com>
2024-11-20 14:05:38 -0300

    Update user.rb

--- spec/internal/app/models/user.rb
+++ spec/internal/app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ::ActiveRecord::Base
   belongs_to :advisor, optional: true
   belongs_to :manager, optional: true
-  has_many :user_organizations, dependent: :destroy, class_name: "::HasHelpers::UserOrganization"
-  has_many :organizations, class_name: "::HasHelpers::Organization", through: :user_organizations
+  has_many :user_organizations, dependent: :destroy, class_name: "::HasHelpers::UserOrganization", inverse_of: :user
+  has_many :organizations, class_name: "::HasHelpers::Organization", through: :user_organizations, inverse_of: :users
 
   def hydrate
   end
