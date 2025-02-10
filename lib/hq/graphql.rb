7193b76130dad456803245f5e875ca769a96ebf5
neryam3 <neryagueromatz@gmail.com>
2025-02-10 11:24:11 -0300

    Update graphql.rb

--- lib/hq/graphql.rb
+++ lib/hq/graphql.rb
@@ -44,7 +44,7 @@ module HQ
        if klass.to_s == "Organization"
           klass = HasHelpers::Organization
        elsif klass.to_s == "User"
-          klass = HasHelpers::User if klass.to_s == "User"
+          klass = HasHelpers::User
        end
       [klass, klass.base_class, klass.superclass].lazy.map do |k|
         config.resource_lookup.call(k) || resources.detect { |r| r.model_klass == k }
